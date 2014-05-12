f123d1512eff189644a4cb9fdf162c35b060064a
vagrant <vagrant@kellogg-dev-box>
2014-05-12 21:26:44 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Homework::Application.config.secret_key_base = '005939fe3a58b3c6ba9b20c086f8d515d37795ddd567454866fd894e84bdf979d62c20576f1cf11bc41b163e5d80931232008867f308f86999d065bf4206815d'
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Homework::Application.routes.draw do
+
+  get "/dice/roll"
+
+  get "/books" => 'books#list'
+  get "/books/:id" => 'books#show'
+
+
+end
